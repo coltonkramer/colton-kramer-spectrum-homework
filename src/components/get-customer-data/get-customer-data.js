c126987509a5f829213821329804efdb31fdaaf6
coltonkramer <77127051+coltonkramer@users.noreply.github.com>
2022-10-17 15:06:20 -0400

    finisehd final touches

--- src/components/get-customer-data/get-customer-data.js
+++ src/components/get-customer-data/get-customer-data.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { calculateCustomerPoints, calculateTotalPoints } from "../../helpers/helpers";
-import './get-customer-data.css'
+import {
+  calculateCustomerPoints,
+  calculateTotalPoints,
+} from "../../helpers/helpers";
+import "./get-customer-data.css";
 
 export default function GetCustomerData() {
-  const [customers, setCustomers] = useState([]);
+  const [customersObject, setCustomersObject] = useState([]);
 
   const fetchCustomerData = async () => {
     const url = "customers.json"; //in public folder
@@ -18,7 +21,7 @@ export default function GetCustomerData() {
   useEffect(() => {
     fetchCustomerData()
       .then((data) => {
-        setCustomers(data.customers);
+        setCustomersObject(data.customers);
       })
       .catch((err) => {
         console.error(err.message);
@@ -27,38 +30,71 @@ export default function GetCustomerData() {
 
   return (
     <div className="Row">
-      {customers.map((customer) => {
+      {customersObject.map((customer) => {
         const monthlyPointsArray = [];
-        
+        const monthlyExpensesArray = [];
+
         //saving the object key to a variable so we don't have to manually call the function more than once
         for (let expense in customer.monthlyExpenses) {
           monthlyPointsArray.push(
             calculateCustomerPoints(customer.monthlyExpenses[expense])
-            );
+          );
+          monthlyExpensesArray.push(customer.monthlyExpenses[expense]);
         }
         return (
-          <div className="Customer-response-container">
+          <div className="Customer-response-container" key={customer.id.toString()}>
             <div className="charter-logo-container">
-              <img src="charter-logo.svg" alt="Charter Communications logo" className="Charter-Logo"></img>
+              <img
+                src="charter-logo.svg"
+                alt="Charter Communications logo"
+                className="Charter-Logo"
+              ></img>
             </div>
-            <p className="Thank-you-text">Thank you for choosing Charter Communications {customer.name}!</p>
-            <div className="Body-text-container">
-            <p>
-              You have received this message because you requested to see how many rewards points you have accumulated over the past 3 months.
+            <p className="Message-Header">
+              Thank you for choosing Charter Communications {customer.name}!
             </p>
-            <p>Below you will find your results.</p>
-            {monthlyPointsArray.map((element, i) => {
-              return (
-                <div key={'div_' + element.toString()}>
-                  <p key={'p_' + element.toString()}>In <span className="Is-bold">Month {i+1}</span> you earned <span className="Is-bold">{element}</span> points</p>
+            <div className="Body-text-container">
+              <p>
+                You have received this message because you requested to see how
+                many rewards points you have accumulated over the past 3 months.
+              </p>
+              <div className="Points-List">
+                <p>Below you will find your results:</p>
+                <div>
+                  <ul>
+                    {monthlyPointsArray.map((pointsEachMonth, i) => {
+                      return (
+                        <li key={i.toString()}>
+                          Month <span>{i + 1}</span>: You spent{" "}
+                          <span className="Is-Bold">
+                            ${monthlyExpensesArray[i]}
+                          </span>{" "}
+                          which earned you{" "}
+                          <span className="Is-Bold">{pointsEachMonth}</span>{" "}
+                          points
+                        </li>
+                      );
+                    })}
+                  </ul>
                 </div>
-              );
-            })}
-            <p>Your total over the last 3 months is <span className="Is-bold">{calculateTotalPoints(monthlyPointsArray)}</span> points</p>
+              </div>
+              <div className="Total-container">
+                <p className="Is-Bold">Total</p>
+                <p>
+                  <span className="Is-Bold">
+                    {calculateTotalPoints(monthlyPointsArray)}
+                  </span>{" "}
+                  points
+                </p>
+              </div>
             </div>
+            <p>To find out how you can redeem and use your rewards points please <span><a href="https://www.spectrum.com/" target="_blank" className="External-Link"> click here</a></span>.</p>
           </div>
         );
       })}
+      <footer>
+        <p>This is intended for educational purposes only</p>
+      </footer>
     </div>
   );
 }
--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -1,7 +1,7 @@
 export const calculateCustomerPoints = (dollarAmountPerMonth) => {
   //Checks if money is less than 100 if its true this calculates only the
   //point for 50 dollars or more if its false it calculates both 100 and 50 dollar points
-  if (dollarAmountPerMonth < 50) return 0;
+  if (dollarAmountPerMonth <= 50) return 0;
   return dollarAmountPerMonth < 100
     ? dollarAmountPerMonth - 50
     : 2 * (dollarAmountPerMonth - 100) + 50;
